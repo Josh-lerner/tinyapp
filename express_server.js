7ba5de42b3c206e96156974f3444545fc1334494
Josh <josh.lerner95@gmail.com>
2020-11-10 02:17:48 +0000

    added 2 redirects, 1 after the post and 1 for long url

--- express_server.js
+++ express_server.js
@@ -2,25 +2,35 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-function generateRandomString() {
+let random = function generateRandomString() {
   return Math.random().toString(36).substring(2,8);
 }
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
-
-app.set("view engine", "ejs");
-
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/urls", (req, res) => {
+  let longUrl = req.body.longURL;  // Log the POST request body to the console;
+  let shortUrl = random();
+  urlDatabase[shortUrl] = longUrl;  
+
+  res.redirect(`/urls/${shortUrl}`);         // Respond with 'Ok' (we will replace this)
+
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  res.redirect(longURL);  // must include http
+});
+
+
+app.set("view engine", "ejs");
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
